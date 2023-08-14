f278bdec7a260c76e4a49b381edc4fae1a0afa68
11Rabbit11 <62289259+11Rabbit11@users.noreply.github.com>
2023-08-14 20:30:20 +0530

    Initial commit

---
+++ be/models/user_model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// User Schema Created - It includes all the required fields.
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    resetToken: { type: String },
+    isAdmin: { type: Boolean, default: false, required: true },
+} , {
+    timestamps : true
+});
+
+//MongoDB Model Created
+const User = mongoose.model('User', userSchema);
+export default User;
+
